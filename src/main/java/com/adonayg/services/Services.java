4121d8706a71e0490ebc72dec8ba61ad3ca25576
adonayg <adonay@tutanota.com>
2018-10-31 10:30:20 +0000

    "new method passess test countByFirstNameTest"

--- src/main/java/com/adonayg/services/Services.java
+++ src/main/java/com/adonayg/services/Services.java
@@ -1,7 +1,9 @@
 package com.adonayg.services;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import com.adonayg.entitiy.Account;
 
@@ -10,6 +12,7 @@ public class Services {
 	private Map<Integer, Account> accountMap = new HashMap<>();
 
 	private Integer id;
+
 
 	public Map<Integer, Account> getAccountMap() {
 		return accountMap;
@@ -31,13 +34,24 @@ public class Services {
 		return account.toString();
 	}
 
-	public Map<Integer, Account> addAccount(Account account) {
+	public Map<Integer, Account> addAccount(int id, Account account) {
 		accountMap.put(id, account);
-		id++;
+
 		return accountMap;
 	}
 
 	public String greetAccount(Account account) {
 		return "Hello " + account.getFirstName();
+	}
+
+	public int countByFirstName(String fname) {
+	
+		int count = 0;
+		for(Account account: accountMap.values()) {
+			if(account.getFirstName().equals(fname)) {
+				count++;
+			}
+		}
+		return count;
 	}
 }
--- src/test/java/com/adonayg/accountapp/test/AccountappTest.java
+++ src/test/java/com/adonayg/accountapp/test/AccountappTest.java
@@ -2,6 +2,9 @@ package com.adonayg.accountapp.test;
 
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -13,17 +16,19 @@ public class AccountappTest {
 
 	@Test
 	public void countByFirstNameTest() {
+
+		
 		Account account1 = new Account("John", "Doe", "123");
 		Account account2 = new Account("Smithy", "Dunn", "134");
 		Account account3 = new Account("John", "Smith", "341");
 		Services service = new Services();
-		
-		service.addAccount(account1);
-		service.addAccount(account2);
-		service.addAccount(account3);
+	
+		service.addAccount(1, account1);
+		service.addAccount(2, account2);
+		service.addAccount(3, account3);
 		
 		int actual = service.countByFirstName("John");
-		Assert.assertEquals(2, actual);
+		assertEquals(2, actual);
 		
 	}
 
