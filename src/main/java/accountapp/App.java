9630eca150f839d21b80851d4a8e88438edcc1a4
adonayg <adonay@tutanota.com>
2018-11-01 09:25:12 +0000

    "java ee project conversion"

--- src/main/java/accountapp/App.java
+++ src/main/java/accountapp/App.java
@@ -1,13 +1,7 @@
 package accountapp;
 
-import java.io.IOException;
-
 import com.adonayg.entitiy.Account;
 import com.adonayg.util.JSONConverter;
-import com.fasterxml.jackson.core.JsonGenerationException;
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class App {
 
